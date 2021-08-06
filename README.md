6f0511217a141cc7f07573c5c7e3ca75c981eee7
Skip2Point0 <68402274+Skip2Point0@users.noreply.github.com>
2021-08-05 22:51:57 -0400

    1) Adding tellib_find_password function. This function allows for different usernames and passwords across all devices. It will also determine whether you have Username set for telnet session or password only. 2) Added ability to use host_ips.txt file for large number of hosts. One host per line. 3) Adjusting readme file to reflect changes brought in with tellib_fine_password function.
    Signed-off-by: Skip2Point0 <68402274+Skip2Point0@users.noreply.github.com>

--- README.md
+++ README.md
@@ -8,15 +8,21 @@ Tested primarily in virtual environment, but should run fine for older series. C
 Requirements:
 
 1) Interpreter: Python 3.8.0+
-2) Python Packages: telnetlib, time
+2) Python Packages: telnetlib, time, re
 
 How to run:
 
 1) Open enable_ssh.py file with text editor and configure variables in the PARAMETERS section:
-   1) Prompt for username is mandatory and script won't run properly without it. Configure user with privilege 15 on 
-      each switch to run this script.
-   2) Enable password is not required, if it is not enabled in switch environment. Leave it as is if no enable needed.
-   3) Rsa Key Modulus length is set to 2048 by default and adjustable via rsa_key_size variable.
+   
+   A) Script supports different usernames and passwords across devices. For telnet, it is able to detect whether Username
+      has been set, or password only.
+   
+   B) Enable password is not required, if it is not enabled in switch environment. Leave it as is if no enable needed.
+   
+   C) Rsa Key Modulus length is set to 2048 by default and adjustable via rsa_key_size variable.
+
+   D) VTY Line #s are set under vty_lines variable, Line 18.
+   
 2) Navigate to script folder. Open terminal and run python3 enable_ssh.py. Once complete, switches should be accessible via SSH. 
 
 Caveat:
--- enable_ssh.py
+++ enable_ssh.py
@@ -1,33 +1,90 @@
 import telnetlib
+import re
 import time
 # ##################################################### PARAMETERS #####################################################
-# Fill out username, password and enable password. If no enable is needed, leave it as is.
-# Switches must prompt for username or the script won't complete.
-# Rsa Key Modulus length is set to 2048 by default.
+# Fill out username, password, enable password, domain name, and vty lines. If no enable is needed, don't make changes
+# to line 11.
+# Script supports different usernames and passwords across devices. For telnet, it is able to detect whether Username
+# has been set, or password only.
+# Rsa Key Modulus length is set to 2048 by default. Change on line 13 if different value is required.
+# By default host ip addresses are entered under line 16. You can use text file host_ip.txt. Comment line 16, and
+# uncomment lines 18-20.
 # ######################################################################################################################
-user = 'cisco'
-password = 'password'
-enable_password = 'password'
+user = ['cisco', 'kisco']
+password = ['password', 'cisco']
+enable_password = ['cisco', 'password']
 domain = "yourveryownenterprises.org"
-host = ['192.168.56.23', '192.168.56.40']
 rsa_key_size = '2048'
+vty_lines = '0 14'
 # ######################################################################################################################
+hosts = ['192.168.56.23', '192.168.56.40']
+# ######################################################################################################################
+# hosts = 'host_ips.txt'
+# with open(hosts) as f:
+#     hosts = f.read().splitlines()
+# ######################################################################################################################
+prompt_list = [b'Username: ', b'Password: ']
 errors = []
 
 
-def apply_ssh(usr, ps, e_ps, hosts, dm, keysize):
-    for line in hosts:
-        print("Configuring host " + line)
-        line = line.strip()
-        try:
-            tn = telnetlib.Telnet(line, timeout=10)
-            tn.read_until(b"Username: ")
-            tn.write(usr.encode('ascii') + b"\n")
-            if ps:
+def tellib_find_password(ls, usr, pasw, en_pasw, swh):
+    tn = telnetlib.Telnet(swh)
+    auth = tn.expect(ls)
+    prompt_parser = re.compile(r'Username:|Password:')
+    prompt_type = prompt_parser.search(auth[1].group(0).decode('ascii'))
+    tn.close()
+    if prompt_type[0] == 'Password:':
+        for p in pasw:
+            tn = telnetlib.Telnet(swh)
+            tn.read_until(b"Password: ")
+            tn.write(p.encode('ascii') + b"\n")
+            try:
+                response = tn.read_until(b">", timeout=1)
+            except EOFError as e:
+                print("Connection closed: %s" % e)
+            if b">" in response:
+                tn.write(b"enable\n")
+                for ep in en_pasw:
+                    tn.write(ep.encode('ascii') + b"\n")
+                    try:
+                        response = tn.read_until(b"#", timeout=1)
+                    except EOFError as e:
+                        print("Connection closed: %s" % e)
+                    if b"#" in response:
+                        return tn
+
+    elif prompt_type[0] == 'Username:':
+        for u in usr:
+            for p in pasw:
+                tn = telnetlib.Telnet(swh)
+                tn.read_until(b"Username: ")
+                tn.write(u.encode('ascii') + b"\n")
                 tn.read_until(b"Password: ")
-                tn.write(ps.encode('ascii') + b"\n")
-            tn.write(b"enable\n")
-            tn.write(e_ps.encode('ascii') + b"\n")
+                tn.write(p.encode('ascii') + b"\n")
+                try:
+                    response = tn.read_until(b">", timeout=1)
+                except EOFError as e:
+                    print("Connection closed: %s" % e)
+                if b">" in response:
+                    tn.write(b"enable\n")
+                    for ep in en_pasw:
+                        tn.write(ep.encode('ascii') + b"\n")
+                        try:
+                            response = tn.read_until(b"#", timeout=1)
+                        except EOFError as e:
+                            print("Connection closed: %s" % e)
+                        if b"#" in response:
+                            return tn
+
+
+def apply_ssh(usr, ps, e_ps, hsts, dm, keysize, vtylines):
+    for host in hsts:
+        print("=================================================")
+        print("Configuring host " + host)
+        print("=================================================")
+        host = host.strip()
+        try:
+            tn = tellib_find_password(prompt_list, usr, ps, e_ps, host)
             tn.write(b"conf t\n")
             print("Assigning Domain")
             tn.write(b"ip domain-name " + dm.encode('ascii') + b"\n")
@@ -36,24 +93,24 @@ def apply_ssh(usr, ps, e_ps, hosts, dm, keysize):
             print("Generating RSA Key")
             tn.write(b"cry key generate rsa general\n")
             time.sleep(3)
-            print("RSA key" + keysize + "Bytes")
+            print("RSA key " + keysize + "Bytes")
             tn.write(keysize.encode('ascii') + b"\n")
             time.sleep(3)
-            print("Setting up VTY lines")
-            tn.write(b"line vty 0 14\n")
+            print("Setting up VTY lines: " + vtylines)
+            tn.write(vtylines.encode('ascii') + b"\n")
             tn.write(b"transport input ssh\n")
             tn.write(b"end\n")
             print("Writing Memory")
             tn.write(b"wr mem\n")
             tn.write(b"exit\n")
             print("DONE")
-            readoutput = tn.read_all().decode('ascii')
-            print(readoutput)
+            print("===============")
+            # print(tn.read_all().decode('ascii'))
         except:
-            errors.append(line)
+            errors.append(host)
 
 
-apply_ssh(user, password, enable_password, host, domain, rsa_key_size)
+apply_ssh(user, password, enable_password, hosts, domain, rsa_key_size, vty_lines)
 
 for error in errors:
-    print("Errors were found on this host: " + error)
+    print("Errors were found on host: " + error)
